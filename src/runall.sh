7bb083336e5840a27ab49b100455d656fdc72a33
Kowser <kowsercse@gmail.com>
2015-02-11 17:55:56 -0600

    one script to rule them all

---
+++ src/runall.sh
@@ -0,0 +1,34 @@
+rm -rf *.out
+
+codes=( naive loop_interchange register prefetch loop_unrolling )
+
+output="size"
+for code in ${codes[@]}
+do
+	# echo matmul_${code}.c
+	output="$output, $code"
+	gcc matmul_${code}.c -O0 -o $code.out
+done
+for code in ${codes[@]}
+do
+	# echo matmul_${code}.c
+	output="$output, $code"
+	gcc matmul_${code}.c -O3 -o ${code}3.out
+done
+echo $output
+
+for size in 400 800 #1200 1600 2000
+do
+	output=$size
+	for code in ${codes[@]}
+	do
+		out=$(./$code.out $size $size $size)
+		output="$output, $out"
+	done
+	for code in ${codes[@]}
+	do
+		out=$(./${code}3.out $size $size $size)
+		output="$output, $out"
+	done
+	echo $output
+done
